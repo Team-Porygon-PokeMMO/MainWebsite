e845c157fa765b7e32d24da8deba5ed2d6659418
Emilio Gomez <emiliogmztj@gmail.com>
2024-02-28 02:43:36 -0800

    Added NuxtJS project.

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <body>
+    <div id="app">
+      <h1>{{ msg }}</h1>
+      <img :src="signatureLink" alt="Pory Team Signature" />
+    </div>
+  </body>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Team Porygon (PokeMMO) WebSite under construction',
+      signatureLink: 'https://i.ibb.co/fq874Kt/Pory-Team-Signature.gif'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: black;
+  color: white;
+}
+</style>
