a845ce72ddedfa905133ce4543ac37f03e042b74
Emilio Gomez <emiliogmztj@gmail.com>
2024-03-31 17:25:13 -0700

    Improved time calc to include new day at 00:00 utc.

--- pages/DayAndNightCycle.vue
+++ pages/DayAndNightCycle.vue
@@ -1,8 +1,15 @@
 <script lang="ts">
+enum Cycle {
+    MORNING = 'Morning 🌅',
+    DAY = 'Day ☀️',
+    NIGHT = 'Night 🌚'
+}
+
 function convertDateToUTC(date: Date) { return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()); }
 let currentTime = convertDateToUTC(new Date());
 
-function addHoursToDate(date: Date, hours: number){
+
+function addHoursToDate(date: Date, hours: number) {
     const newHours = date.getHours() + hours;
     date.setHours(newHours);
     return date;
@@ -25,39 +32,40 @@ let dayEnd: Date = new Date(currentTime);
 dayEnd.setHours(7);
 dayEnd.setMinutes(0);
 dayEnd.setSeconds(0);
+let currentCycle: Cycle = Cycle.MORNING;
 
-if(currentTime >= dayEnd){
-    const getCycle = Math.floor(currentTime.getHours() / 6)
-    morningStart = addHoursToDate(morningStart, 6 * getCycle);
-    dayStart = addHoursToDate(dayStart, 6 * getCycle);
-    nightStart = addHoursToDate(nightStart, 6 * getCycle);
-    dayEnd = addHoursToDate(dayEnd, 6 * getCycle);
+if (currentTime <= morningStart) {
+    currentCycle = Cycle.NIGHT;
+}
+else {
+    if (currentTime >= dayEnd) {
+        const getCycle = Math.floor(currentTime.getHours() / 6)
+        morningStart = addHoursToDate(morningStart, 6 * getCycle);
+        dayStart = addHoursToDate(dayStart, 6 * getCycle);
+        nightStart = addHoursToDate(nightStart, 6 * getCycle);
+        dayEnd = addHoursToDate(dayEnd, 6 * getCycle);
+    }
+
+    if (currentTime >= morningStart && currentTime >= dayStart) {
+        morningStart = addHoursToDate(morningStart, 6);
+    }
+    if (currentTime >= dayStart && currentTime >= nightStart) {
+        dayStart = addHoursToDate(dayStart, 6);
+    }
+    console.dir(dayEnd)
+    if (currentTime >= nightStart && currentTime >= morningStart) {
+        nightStart = addHoursToDate(nightStart, 6);
+    }
+    if (currentTime >= dayEnd) {
+        dayEnd = addHoursToDate(dayEnd, 6);
+    }
 }
 
-if(currentTime >= morningStart && currentTime >= dayStart){
-    morningStart = addHoursToDate(morningStart, 6);
-}
-if(currentTime >= dayStart && currentTime >= nightStart){
-    dayStart = addHoursToDate(dayStart, 6);
-}
-if(currentTime >= nightStart && currentTime >= dayEnd){
-    nightStart = addHoursToDate(nightStart, 6);
-}
-if(currentTime >= dayEnd){
-    dayEnd = addHoursToDate(dayEnd, 6);
-}
-
-enum Cycle {
-    MORNING = 'Morning 🌅',
-    DAY = 'Day ☀️',
-    NIGHT = 'Night 🌚'
-}
-
-function getNextCycles(){
+function getNextCycles() {
     let cycles = [
-        { cycle: Cycle.MORNING, time: morningStart, current: morningStart <= currentTime && currentTime < dayStart},
-        { cycle: Cycle.DAY, time: dayStart, current: dayStart <= currentTime && currentTime < nightStart},
-        { cycle: Cycle.NIGHT, time: nightStart, current: nightStart <= currentTime && currentTime < dayEnd},
+        { cycle: Cycle.MORNING, time: morningStart, current: currentCycle === Cycle.MORNING},
+        { cycle: Cycle.DAY, time: dayStart, current: currentCycle === Cycle.DAY},
+        { cycle: Cycle.NIGHT, time: nightStart, current: currentCycle === Cycle.NIGHT},
     ];
     return cycles;
 }
@@ -70,21 +78,34 @@ export default {
         };
     },
     computed: {
-        currentTime : () => currentTime
+        currentTime: () => currentTime
     },
     methods: {
         getNextCycles,
-        getTimeLeft(toCalculateDate: Date){
-            const diff = toCalculateDate.getTime() - currentTime.getTime();
-            const hours = Math.floor(diff / 1000 / 60 / 60);
-            const minutes = Math.floor(diff / 1000 / 60 % 60);
-            if(hours <= 0){
-                return `Started ${Math.abs(minutes)} minutes ago.`;
+        getTimeLeft(cycle: Cycle, toCalculateDate: Date) {
+            let diff = toCalculateDate.getTime() - currentTime.getTime();
+            let hours = Math.floor(diff / 1000 / 60 / 60);
+            let minutes = Math.floor(diff / 1000 / 60 % 60);
+            if (cycle == currentCycle) {
+                let nextCycleStart = dayStart
+                switch(cycle){
+                    case Cycle.MORNING:
+                        nextCycleStart = dayStart;
+                        break
+                    case Cycle.DAY:
+                        nextCycleStart = nightStart;
+                        break
+                    case Cycle.NIGHT:
+                        nextCycleStart = morningStart;
+                        break
+                }
+
+                diff = nextCycleStart.getTime() - currentTime.getTime();
+                hours = Math.floor(diff / 1000 / 60 / 60);
+                minutes = Math.floor(diff / 1000 / 60 % 60);
+                return 'ends in ' + (hours == 0 ? `in ${minutes} minutes` : `in ${hours} hours and ${minutes} minutes`) + '.';
             }
-            return `in ${hours} hours and ${minutes} minutes`;
-        },
-        refreshCurrentTime(){
-            currentTime = convertDateToUTC(new Date());
+            return 'in ' + (hours == 0 ? `${minutes} minutes` : `${hours} hours and ${minutes} minutes`) + '.';
         }
     },
 };
@@ -95,8 +116,13 @@ export default {
         <h1>Day and Night Cycle</h1>
         <h2>Current Cycle: {{ currentTime }}</h2>
         <h2 v-for="cycle in getNextCycles()" :key="cycle.cycle" :class="{ isActive: cycle.current }">
-            {{ cycle.cycle }} {{ getTimeLeft(cycle.time) }} {{ cycle.current ? '(Current)' : '' }}
+        {{ cycle.cycle }} {{ getTimeLeft(cycle.cycle, cycle.time) }} 
         </h2>
-        <UButton @click="refreshCurrentTime()">Refresh</UButton>
     </div>
 </template>
+
+<style scoped>
+.isActive{
+    color: lightcoral;
+}
+</style>
