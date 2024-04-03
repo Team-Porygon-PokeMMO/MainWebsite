89a07244c41d32bf8773c7c900bb985d822792a4
Emilio Gomez <emiliogmztj@gmail.com>
2024-04-03 02:48:53 -0700

    Updated design plus a stop timer. Interval 5 imes per second.

--- pages/HatchCycleTimer.vue
+++ pages/HatchCycleTimer.vue
@@ -1,9 +1,8 @@
-
-
 <script lang="ts">
-let currentTimer = null
-let endTimer = null
+let currentTimer: Date | null = null
+let endTimer: Date | null = null
 let volume = 50
+let timer = null
 export default {
     data() {
         return {
@@ -14,12 +13,12 @@ export default {
             pingSecond: 0,
             eggHatchSecond: 6,
             remainingTimer: '',
-            volume: 50
+            volume: 50,
         };
     },
     mounted() {
         this.updateTime();
-        setInterval(this.updateTime, 1000);
+        timer = setInterval(this.updateTime, 200);
     },
     methods: {
         updateTime() {
@@ -29,7 +28,7 @@ export default {
             }
         },
         checkTimer() {
-            if (this.endTimer == '') {
+            if (this.endTimer == null) {
                 return
             }
             let currentTimer = new Date(this.currentTimer)
@@ -79,12 +78,18 @@ export default {
             this.endTimer = endTimer
         },
         sendAlert() {
-            this.playAudio()
+            if (this.timerSet) {
+                this.playAudio()
+            }
         },
         playAudio() {
             const audio = new Audio('https://cdn.freesound.org/previews/352/352651_4019029-lq.ogg');
             audio.volume = this.volume / 100;
             audio.play();
+        },
+        stopTimer() {
+            this.endTimer = new Date()
+            this.timerSet = false
         }
     }
 };
@@ -107,29 +112,52 @@ button {
 
 <template>
     <UContainer>
-        <h1>Hatch Cycle Timer</h1>
-        <p v-if="currentTimer != null">Current Time: {{ currentTimer.toLocaleTimeString() }}</p>
-        <br />
-        <p>Egg Hatch Second: <b>{{ eggHatchSecond }}</b></p>
-        <UInput v-model="eggHatchSecond" />
-        <br />
-        <p>Timer (starts from last Egg Hatch Second): </p>
-        <UInput v-model="pingMinute" type="number" placeholder="Minutes" />
-        <UInput v-model="pingSecond" type="number" placeholder="Seconds" />
-        <br />
-        <UButton @click="startTimer()">Start</UButton>
-        <UButton @click="missedInterval()">Missed Interval</UButton>
-        <div v-if="timerSet">
-            <h1 v-if="endTimer != null">End Timer: {{ endTimer.toLocaleTimeString() }}</h1>
-            <h1>Timer ends in {{ remainingTimer }}.</h1>
-        </div>
         <div>
+            <h1>Hatch Cycle Timer</h1>
+            <div class="grid gap-2 mb-2 md:grid-cols-1 text-center">
+                <h1 v-if="currentTimer != null">Current Time: {{ currentTimer.toLocaleTimeString() }}</h1>
+            </div>
+            <div class="grid gap-2 mb-2 md:grid-cols-1">
+                <label>Egg-Hatching Second:</label>
+                <UInput v-model="eggHatchSecond" />
+            </div>
+            <h2>Timer (starts from last Egg Hatch Second): </h2>
+            <div class="grid gap-4 mb-2 md:grid-cols-2">
+                <div>
+                    <label>Minutes</label>
+                    <UInput v-model="pingMinute" type="number" placeholder="Minutes" />
+                </div>
+                <div>
+                    <label>Seconds</label>
+                    <UInput v-model="pingSecond" type="number" placeholder="Seconds" />
+                </div>
+                <div>
+                    <UButton @click="startTimer()">Start</UButton>
+                    <UButton @click="stopTimer()">Stop</UButton>
+                    <UButton @click="missedInterval()">Missed Interval</UButton>
+                </div>
+            </div>
+            <div class="grid md:grid-cols-1" v-if="timerSet">
+                <div class="text-center">
+                    <h1 v-if="endTimer != null">End Timer: {{ endTimer.toLocaleTimeString() }}</h1>
+                </div>
+                <div class="text-center">
+                    <h3>Timer ends in {{ remainingTimer }}.</h3>
+                </div>
+            </div>
             <h1>Notification Style:</h1>
-            <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Audio
-                Volume</label>
-            <input id="default-range" v-model="volume" type="range"
-                class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
-            <UButton @click="playAudio()">Test Audio</UButton>
+            <div class="grid gap-2 mb-2 md:grid-cols-1">
+                <div>
+                    <label for="default-range"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Audio
+                        Volume</label>
+                    <input id="default-range" v-model="volume" type="range"
+                        class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+                </div>
+                <div>
+                    <UButton @click="playAudio()">Test Audio</UButton>
+                </div>
+            </div>
         </div>
     </UContainer>
 </template>
