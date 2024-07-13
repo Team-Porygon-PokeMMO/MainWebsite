cb96a933d9c53d63b06e89c91cdcc17c06e9f78c
Emilio Gomez <emiliogmztj@gmail.com>
2024-07-13 14:56:10 -0700

    Added logic to show extra steps recursively.

---
+++ components/Steps.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { Step } from '~/types/EliteFour.types.vue'
+export default {
+    props: {
+        currentStep: {
+            type: Step,
+            required: true,
+        },
+    },
+    methods: {
+        toggleView() {
+            this.currentStep.IsVisible = !this.currentStep.IsVisible
+        },
+        getIfExpandable() {
+            return this.currentStep?.Steps?.length > 0 ? '▶' : ''
+        },
+    },
+};
+</script>
+
+<style scoped>
+/* Add your component styles here */
+</style>
+
+<template>
+    <div class="pl-10 pt-2">
+        <ul>
+            <li @click="toggleView()">
+                {{ getIfExpandable() }} {{ currentStep.Description }}
+            </li>
+        </ul>
+        <Steps v-for="subStep in currentStep.Steps" :currentStep="subStep" v-show="currentStep.IsVisible" />
+    </div>
+</template>
---
+++ types/EliteFour.types.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+export class Step {
+    Description: string = "";
+    Classes: string[] = [];
+    Steps: Step[] = [];
+    IsVisible: boolean = false;
+}
+export class Trainer {
+    Name: string = "";
+    IsVisible: boolean = false;
+    Leads: Leads[] = [];
+}
+export class Leads {
+    Name: string = "";
+    IsVisible: boolean = false;
+    Steps: Step[] = [];
+}
+export class Region {
+    Name: string = "";
+    IsVisible: boolean = false;
+    GymTrainers: Trainer[] = [];
+}
+</script>
