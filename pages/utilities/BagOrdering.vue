2bb92c441a5e02de81bdab1aeef7dcb3803e8be3
Emilio Gomez <emiliogmztj@gmail.com>
2024-11-22 20:56:45 -0800

    Added mock up.

--- pages/utilities/BagOrdering.vue
+++ pages/utilities/BagOrdering.vue
@@ -5,6 +5,12 @@ export default {
         };
     },
     methods: {
+        sortUp() {
+            console.dir("sortUp")
+        },
+        sortDown() {
+            console.dir("sortDown")
+        }
     },
 
 };
@@ -32,10 +38,26 @@ export default {
 </style>
 
 <template>
-    <div class="m-4 main">
-        <div class="p-2 text-center">
-            <h2>Bag Ordering</h2>
-            <h3>Create your own bag sorting with this utility.</h3>
+    <UContainer>
+        <div class="m-4 main">
+            <div class="p-2 text-center">
+                <h2>Bag Ordering</h2>
+                <h3>Create your own bag sorting with this utility.</h3>
+                <div class="grid grid-cols-12 grid-rows-1 text-center">
+                    <div class="col-span-1">
+                        <UIcon name="i-heroicons-arrow-long-up" class="text-xl cursor-pointer" @click="sortUp" />
+                    </div>
+                    <div class="col-span-1">
+                        <UIcon name="i-heroicons-arrow-long-down" class="text-xl cursor-pointer" @click="sortDown" />
+                    </div>
+                    <div class="col-span-3">
+                        <UInput placeholder="Priority" />
+                    </div>
+                    <div class="col-span-7">
+                        <USelect placeholder="Item Name" />
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    </UContainer>
 </template>
