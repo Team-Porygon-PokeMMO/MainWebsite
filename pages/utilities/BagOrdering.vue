c94aa15101dc1e2fa74bc3aed2cac3ab4546314c
Emilio Gomez <emiliogmztj@gmail.com>
2024-11-22 20:27:05 -0800

    Starting page.

---
+++ pages/utilities/BagOrdering.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+
+};
+</script>
+
+<style>
+.main {
+    min-height: 100%;
+}
+
+.active {
+    background-color: rgba(51, 3, 3, 0.95);
+}
+
+/* Responsive styles */
+@media (max-width: 768px) {
+    .menu {
+        padding: 10px;
+    }
+
+    .menu-list a {
+        padding: 8px;
+    }
+}
+</style>
+
+<template>
+    <div class="m-4 main">
+        <div class="p-2 text-center">
+            <h2>Bag Ordering</h2>
+            <h3>Create your own bag sorting with this utility.</h3>
+        </div>
+    </div>
+</template>
