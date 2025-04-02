f3e83d4e1af8d2d82f2fc988b5428ddb6c27828d
Jade <White.kevin732@gmail.com>
2025-04-02 12:01:46 -0600

    Resolved most issues, introduced issue in notTrainers

--- components/NotLeads.vue
+++ components/NotLeads.vue
@@ -4,9 +4,12 @@ import nameToDex from '~/src/nameToDex.json'
 
 class Lead {
     Name: string = "";
+    IsGenSelected: boolean = false;
+    IsPokemonStartSelected: boolean = false;
     IsVisible: boolean = false;
     Steps: Step[] = [];
 }
+
 class Step {
     Description: string = "";
     Classes: string[] = [];
@@ -26,34 +29,57 @@ export default {
     data() {
         return {
             nameToDex: nameToDex as IDictionary
+        };
+    },
+    computed: {
+        typedItems(): Lead[] {
+            return this.items as Lead[];
         }
     },
     methods: {
+        setGenActive(item: Lead) {
+            this.typedItems.forEach((i) => i !== item ? i.IsGenSelected = false : i.IsGenSelected = !item.IsGenSelected);
+        },
+        setPokemonStartActive(item: Lead) {
+            this.typedItems.forEach((i) => i !== item ? i.IsPokemonStartSelected = false : i.IsPokemonStartSelected = !item.IsPokemonStartSelected);
+        },
         setActive(item: Lead) {
-            this.items.forEach((i) => i !== item ? i.IsVisible = false : i.IsVisible = !item.IsVisible);
+            this.typedItems.forEach((i) => i !== item ? i.IsVisible = false : i.IsVisible = !item.IsVisible);
         },
         getImageStyling(item: Lead) {
-            let imgNumber: string = this.nameToDex[item.Name]
+            let imgNumber: string = this.nameToDex[item.Name];
             return {
                 backgroundImage: `linear-gradient(rgb(0, 0, 0, 0.4), rgba(0, 0, 0, 0)), url(/images/leads/${imgNumber}.png)`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
-            }
+            };
         }
-    },
+    }
 };
 </script>
 
 <template>
     <div class="text-center">
         <UDivider class="my-2" />
-        <div class="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 lg:py-0.5 grid-cols-5 cursor-pointer">
-            <div v-for="item in items" @click="setActive(item)" :class="{ active: item.IsVisible }"
-                class="py-1 sm:text-sm sm:py-2 border border-black m-0.25 md:text-base md:py-3 lg:py-5 hover:bg-cyan-700" 
+        
+        <!-- First Grid: Gen Selection -->
+        <div class="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-5 cursor-pointer">
+            <div v-for="item in items" @click="setGenActive(item)" :class="{ active: item.IsGenSelected }"
+                class="py-1 sm:text-sm sm:py-2 border border-black m-0.25 md:text-base md:py-3 lg:py-5 hover:bg-cyan-700"
                 :style="getImageStyling(item)">
                 <span class="bg-black bg-opacity-50 text-white p-0.5">{{ item.Name }}</span>
             </div>
         </div>
-        <Steps class="mt-4" v-for="item in items" :items="item.Steps" v-show="item.IsVisible">test</Steps>
+
+        <!-- Second Grid: Pokemon Start (only visible when Gen is selected) -->
+        <div v-if="items.some(i => i.IsGenSelected)" class="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-5 cursor-pointer mt-4">
+            <div v-for="item in items" @click="setPokemonStartActive(item)" :class="{ active: item.IsPokemonStartSelected }"
+                class="py-1 sm:text-sm sm:py-2 border border-black m-0.25 md:text-base md:py-3 lg:py-5 hover:bg-cyan-700">
+                <span class="bg-black bg-opacity-50 text-white p-0.5">Pokemon Start</span>
+            </div>
+        </div>
+
+        <!-- Steps: only visible when Pokemon Start is selected -->
+        <Steps class="mt-4" v-for="item in items" :items="item.Steps" v-show="item.IsPokemonStartSelected">test</Steps>
     </div>
 </template>
